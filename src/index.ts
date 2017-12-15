1bf5d3755be6707a1a2cd9e9c55c2b1327dc53a2
Kenneth Chau <Kenneth.Chau@massive.co>
2017-12-15 16:31:07 +0000

    added multiple service in docker compose

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { Request, Response, Next, createServer } from "restify";
 import * as os from 'os';
+import * as redis from 'redis';
 
 function respond(req: Request, res: Response, next: Next) {
-    res.send('hello ' + req.params.name + " from " + os.hostname);
-    next()
+    let r = redis.createClient({
+        host: "redis",
+        db: 0,
+        connect_timeout: 2
+    });
+    
+    r.incr("counter", (err, number) => {
+        if (err)
+            res.send('hello ' + req.params.name + " from " + os.hostname + os.EOL
+                + "visits: " + "redis error");
+        else
+            res.send('hello ' + req.params.name + " from " + os.hostname + os.EOL
+                + "visits: " + number);
+        next();
+    });
 }
 
 var server = createServer();
